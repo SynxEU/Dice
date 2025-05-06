2a2f41d02cf24e036dd499670a8cd355ae6b16b2
Jonas Egetoft Knudsen <jonkn0001@elevcampus.dk>
2025-05-06 10:12:32 +0200

    Initial Commit

---
+++ BlazorApp1/Components/Model/DiceModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp1.Components.Model;
+
+public class DiceModel
+{
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    public TimeSpan Time { get; set; } = DateTime.UtcNow.TimeOfDay;
+}
